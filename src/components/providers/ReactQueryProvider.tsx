331cf6dde08f91b1726ebba2db7f87a4ce08a5ac
Maksym <tarasovcad@gmail.com>
2026-03-31 20:25:28 -0500

    fix(query): update provider for robust SSR support

--- src/components/providers/ReactQueryProvider.tsx
+++ src/components/providers/ReactQueryProvider.tsx
@@ -1,10 +1,30 @@
 "use client";
 
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {useState} from "react";
+import {QueryClient, QueryClientProvider, isServer} from "@tanstack/react-query";
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (isServer) {
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
 
 export function ReactQueryProvider({children}: {children: React.ReactNode}) {
-  const [queryClient] = useState(() => new QueryClient());
+  const queryClient = getQueryClient();
 
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
